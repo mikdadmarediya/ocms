0be294341c985abefc9a4aefd7c4a04d35c3a7a9
mikdadmarediya <mikdad.marediya@gmail.com>
2021-03-25 17:59:34 +0530

    initial commit1

---
+++ demo/src/test/java/com/ocms/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocms.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
